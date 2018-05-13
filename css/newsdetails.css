95667f5d17b016a4a385f609e797e0360dc96396
GXS1225 <1396795023@qq.com>
2018-05-13 17:46:57 +0800

    Commit Project Demo

---
+++ css/newsdetails.css
@@ -0,0 +1,349 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<TITLE>小智没找到您想要的内容！_山东成人高考报名网_潍坊成人高考专升本函授站_潍坊华智教育</TITLE>
+<meta name="keywords" content="小智,没,找到,您想,要的,内容," />
+<meta name="description" content="" />
+ 
+    <script  src="/Public/css+js/jquery.js"></script>
+    <link rel="stylesheet" type="text/css" href="/Public/css+js/css.css" />
+
+<link rel="stylesheet" type="text/css" href="/Public/css+js/newsdetails.css" />
+  <script src="/Public/css+js/jquery-1.7.2.min.js"></script>   
+</head>
+<body> 
+
+<script>
+(function(){
+    var bp = document.createElement('script');
+    var curProtocol = window.location.protocol.split(':')[0];
+    if (curProtocol === 'https') {
+        bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';        
+    }
+    else {
+        bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+    }
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(bp, s);
+})();
+</script>
+
+
+
+<div class="topbox">
+<a href="/" alt="山东成人高考" title="潍坊华智教育"><img class="l"src="/Public/images/logo.jpg" alt="山东专升本" height="88" width="615" alt="潍坊华智教育"></a>
+<i class="r"></i>
+</div>
+
+<div class="navbox">
+	<div class="nav1 c1earbox">
+    	<span class="list"><a href="/" class="a1" style="background:url(/Public/images/arrow2.png) no-repeat 184px center #d1102c" alt="潍坊成人高考" title="成人教育方案">学历提升计划</a>
+        	<ul class="ul1 clearbox">
+			
+            	 <a href="/kecheng/zhuanshengben/"><li >专升本</li></a>
+			
+            	 <a href="/kecheng/gaoshengzhuan/"><li >高起专/高起本</li></a>
+			
+            	 <a href="/kecheng/jingjixueshuoshi/"><li >山东财经大学-经济学硕士</li></a>
+			
+            	 <a href="/kecheng/guanlixueshuoshi/"><li >山东财经大学-管理学硕士</li></a>
+			
+            	 <a href="/kecheng/yixueshuoshi/"><li >青岛大学医学院-医学硕士</li></a>
+				
+            </ul>
+        </span>
+        <a href="/" alt="潍坊专升本" title="潍坊华智教育"><span>首页</span></a>
+        <a href="/about" alt="潍坊成人高考" title="潍坊华智教育"><span>关于华智</span></a>
+                
+        
+       
+         <span class="list1" style="padding:0px 0px; position:relative;"><a href="/huazhixiangce" alt="山东成人高考" title="潍坊华智教育"><span>华智相册</span></a>
+        	<ul class="ul2 clearbox" style="position:absolute; top:50px; left:0px;">
+            
+            	<a href="/huazhixiangce/2011/" alt="2011年" title="2011年"><li>2011年</li></a>
+             
+            	<a href="/huazhixiangce/2012/" alt="2012年" title="2012年"><li>2012年</li></a>
+             
+            	<a href="/huazhixiangce/2013/" alt="2013年" title="2013年"><li>2013年</li></a>
+             
+            	<a href="/huazhixiangce/2014/" alt="2014年" title="2014年"><li>2014年</li></a>
+             
+            	<a href="/huazhixiangce/2015/" alt="2015年" title="2015年"><li>2015年</li></a>
+             
+            	<a href="/huazhixiangce/2016/" alt="2016年" title="2016年"><li>2016年</li></a>
+             
+            	<a href="/huazhixiangce/2017nian/" alt="2017年" title="2017年"><li>2017年</li></a>
+             	
+            </ul>
+        </span>
+        
+        
+        
+        
+        
+        
+        <span class="list1" style="padding:0px 0px;"><a href="/teacher/" class="a2" alt="山东专升本" title="潍坊华智教育">华智风采</a>
+        	<ul class="ul2 clearbox">
+            	<a href="/teacher/index.html" alt="潍坊专升本" title="潍坊华智教育"><li>教师风采</li></a>
+                <a href="/xueyuanfengcai/" alt="潍坊成人高考" title="潍坊华智教育"><li>学员风采</li></a>
+            </ul>
+        </span>
+        <a href="/faq/" alt="山东成人高考" title="成人高考问题"><span>报考常识</span></a>
+        <a href="/news/" alt="山东专升本" title="潍坊华智教育"><span>学院动态</span></a>
+        <a href="/contact/" alt="山东成人高考" title="成人高考报名"><span>联系我们</span></a>
+	</div>
+</div>
+
+<div class="mainbox" >   
+<div class="main2 clearbox">
+<div class="tit2">     潍坊华智教育 > 404页面小智没找到您想要的内容！</div>
+<div class="left2 l">
+	
+    	<div class="i_con18 l">
+        	<div class="a1" >小智没找到您想要的内容！</div>
+            <div class="ss clearbox">
+                <i class="l">更新时间：2016-10-12    来源:华智教育在职研究生    浏览次数： <script src="/plus/count.php?view=yes&aid=172&mid=1" type='text/javascript' language="javascript"></script> </i>
+                <ul class="r">
+                    <li style="margin-right:6px;">分享到:</li>
+                    <li><div style="margin-top:4px;"class="bshare-custom"><a title="分享到新浪微博" class="bshare-sinaminiblog"></a><a title="分享到QQ空间" class="bshare-qzone"></a><a title="分享到微信" class="bshare-weixin"></a><a title="分享到人人网" class="bshare-renren"></a><a title="更多平台" class="bshare-more bshare-more-icon more-style-addthis" style="margin-right:0px;"></a></div></li>
+                 </ul>
+             </div>
+             <div class="textbox clearbox" >
+                <div style="text-align: center;">
+	<img alt="华智教育页面" src="/uploads/allimg/161012/1-161012120116312.jpg" style="width: 790px; height: 440px;" /></div>
+
+			 </div>
+              <div class="ss clearbox" style="margin-bottom:40px;margin-top:10px;">
+               <ul class="l" >
+                    <li style="margin-right:6px;">分享到:</li>
+                    <li><div style="margin-top:4px;"class="bshare-custom"><a title="分享到新浪微博" class="bshare-sinaminiblog"></a><a title="分享到QQ空间" class="bshare-qzone"></a><a title="分享到微信" class="bshare-weixin"></a><a title="分享到人人网" class="bshare-renren"></a><a title="更多平台" class="bshare-more bshare-more-icon more-style-addthis"></a></div></li>
+                 </ul>
+            </div>
+            <div style="  height:30px; line-height:30x; font-family:'微软雅黑'; font-size:14px; color:#333;"><span style="color:#333;">上一篇：没有了 </span></div>
+            <div style="  height:30px; line-height:30x; font-family:'微软雅黑'; font-size:14px;color:#333;"><span style="color:#333;">下一篇：没有了 </span></div>
+         </div>
+</div>
+<div class="right1 r">
+	<img src="/Public/images/image1.jpg" />
+    <div class="i_con48 clearbox " >
+	<div class="tit4 clearbox">
+	  <p class="l">开班信息</p><a  class="r" href="/zhaoshengjianzhang/">更多>></a></div>
+	<ul class="clearbox">
+              <a href="/plus/view.php?aid=100"><li class="li2">3月24日：青岛大学医学院医学硕士</li></a>
+<a href="/plus/view.php?aid=101"><li class="li2">3月24日：青岛科技大学工程硕士开</li></a>
+<a href="/plus/view.php?aid=102"><li class="li2">3月24日：青岛科技大学MBA</li></a>
+<a href="/plus/view.php?aid=103"><li class="li2">3月15日：河南师范大学教育硕士开</li></a>
+<a href="/plus/view.php?aid=104"><li class="li2">3月15日：河南师范大学艺术硕士开</li></a>
+<a href="/plus/view.php?aid=105"><li class="li2">3月2日：山东财经大学经济硕士开</li></a>
+  
+      	</ul>
+    </div>
+	<img src="/Public/images/image2.jpg" style="margin-top:10px;" />
+    <div class="i_con48 clearbox " >
+	<div class="tit4 clearbox"><p class="l">政策速递</p><a  class="r" href="/consult/">更多>></a></div>
+	<ul class="clearbox">
+	<li class="li2"><a href="/consult/399.html">2017山东成人高考潍坊学院招生简</a></li>
+<li class="li2"><a href="/consult/397.html">注意！2017山东成人高考迎来改革</a></li>
+<li class="li2"><a href="/consult/394.html">2018年学历继续教育完成三库合一</a></li>
+<li class="li2"><a href="/consult/381.html">2018山东成人教育难度将加大</a></li>
+<li class="li2"><a href="/consult/376.html">要闻：大力发展继续教育 加快学</a></li>
+			
+         	</ul>
+    <div class="tit4 clearbox"><p class="l">学院动态</p><a  class="r" href="/news/">更多>></a></div>
+    <ul class="clearbox">
+     <li class="li2"><a href="/news/395.html">华智教育教师资格证小课堂圆满结</a></li>
+<li class="li2"><a href="/news/325.html">潍坊华智教育2017级新生开学典礼</a></li>
+<li class="li2"><a href="/news/320.html">西科大学网络教育入学考试在潍坊</a></li>
+<li class="li2"><a href="/news/282.html">山财在职研究生开班典礼本将于本</a></li>
+<li class="li2"><a href="/news/279.html">山农大2016下学期期末考试在潍坊</a></li>
+
+             </ul>
+    <div class="tit4 clearbox"><p class="l">招生问答</p><a  class="r" href="/faq">更多>></a></div>
+    <ul class="clearbox">
+        <li class="li2"><a href="/faq/chengrengaokao/400.html">2017年毕业生工资大曝光，谁说学</a></li>
+<li class="li2"><a href="/faq/chengrengaokao/398.html">2017年山东成人高考：先专升本还</a></li>
+<li class="li2"><a href="/faq/chengrengaokao/396.html">外地考生参加山东成人高考报名是</a></li>
+<li class="li2"><a href="/faq/chengrengaokao/392.html">山东成人高考—怎么查询自己的学</a></li>
+<li class="li2"><a href="/faq/chengrengaokao/390.html">山东成人高考函授的学习形式</a></li>
+
+             </ul>
+    </div>
+	</div>
+    
+   
+</div>
+</div>
+
+<div class="foot">
+<div class="con">
+<h2>成人高考、在职研究生报名方式</h2>
+<ul class="kf">
+<li class="b1"><a href="http://p.qiao.baidu.com/im/index?siteid=9906026&ucid=22330209&cp=&cr=&cw=" target="_blank" alt="在职研究生报名" title="在职研究生报名">在线咨询</a></li>
+<li class="b2"><a href="http://wpa.qq.com/msgrd?v=3&uin=3346298332&site=qq&menu=yes" target="_blank" alt="在职研究生报名" title="在职研究生报名">QQ客服</a></li>
+<li class="b3"><a>400-626-3637</a></li>
+</ul>
+<div class="foot_map l">
+<p>潍坊华智教育地图导航</p>
+<iframe src="/map.htm" height="262px" width="372px"></iframe>
+</div>
+<ul class="rwm r">
+<p></p>
+<li><img src="/Public/images/wxewm.gif" alt="在职研究生报名" title="在职研究生报名"><b>获取成人高考最新信息</b></li>
+<li><img src="/Public/images/wxewm2.gif" alt="在职研究生报名" title="在职研究生报名"><b>关注华智教育天猫旗舰店</b></li>
+</ul>
+<div class="foot_baom r">
+<p>如有问题，可在线提交表单</p>
+<form action="/plus/diy.php" enctype="multipart/form-data" method="post" onSubmit="return addbaoming()">
+<input type="hidden" name="action" value="post" />
+<input type="hidden" name="diyid" value="1" />
+<input type="hidden" name="do" value="2" />
+<ul class="bg">
+<li><input name="truename" id="truename" class="xm" placeholder="真实姓名" type="text"></li>
+<li><input name="tel" id="tel" class="dh" placeholder="电话" type="text"></li>
+<li><input name="qqkefu" id="qq" class="qq" placeholder="QQ" type="text"></li>
+
+<li><input name="" value="点击提交" class="djtj" type="submit" ></li>
+<li class="ts">提交后，我们的老师会及时为您回复</li>
+</ul>
+<input type="hidden" name="dede_fields" value="truename,text;tel,text;city,textchar;qqkefu,text" />
+<input type="hidden" name="dede_fieldshash" value="2618cacca33232c731be18a72b6dec16" />
+</div>
+</div>
+
+
+<div class="foot_nav">
+<ul class="one">
+<li><a href="/" alt="潍坊华智教育" title="潍坊华智教育">首页</a></li><li>
+</li>
+
+<li>
+<a href="/about/" target="_blank" alt="潍坊华智教育" title="潍坊华智教育">关于华智</a>
+<ul class="two">
+
+<li><a href="/consult/">政策速递</a></li>
+
+<li><a href="/about/gonggao/">学习公告</a></li>
+
+<li><a href="/huanjing/">校园环境</a></li>
+ 
+</ul>
+</li>
+<li>
+<a href="/zhaoshengjianzhang/shandongligongdaxue/" alt="成人教育招生" title="成人教育招生">招生简章</a>
+</li>
+<li>
+<a href="/teacher/" alt="潍坊华智教育" title="潍坊华智教育">华智风采</a>
+<ul class="two">
+
+<li><a href="/teacher/">教师风采</a></li>
+
+<li><a href="/xueyuanfengcai/">学员风采</a></li>
+ 
+</ul>
+</li>
+<li>
+<a href="http://www.huazhijiaoyu.com/zhaoshengjianzhang/" target="_blank" alt="成人教育开班" title="成人教育招生">开班信息</a>
+<ul class="two">
+
+<li><a href="/zhaoshengjianzhang/chengrenzhuanbenban/">成人专\本班</a></li>
+
+<li><a href="/zhaoshengjianzhang/zaizhiyanjiushengban/">在职研究生班</a></li>
+ 
+</ul>
+</li>
+<li>
+<a href="/faq" target="_blank" alt="成人教育问答" title="成人教育问答">招生问答</a>
+</li>
+<li>
+<a href="/huazhixiangce" target="_blank" alt="潍坊华智教育" title="潍坊华智教育">华智相册</a>
+<ul class="two">
+</ul>
+</li>
+<li>
+<a href="/news" target="_blank" alt="成人教育最新动态" title="成人教育最新动态">学院动态</a>
+<ul class="two">
+
+ 
+</ul>
+</li>
+<li>
+<a href="/contact" target="_blank" alt="潍坊华智教育" title="潍坊华智教育">联系我们</a>
+</li>
+</ul>
+<p>潍坊华智教育科技开发有限公司</p>
+
+<div class="f_link">
+  <h3><a class="title" rel="nofollow"><center>友情链接</center></a></h3>
+  <div class="link_cnt"><center> <a href='http://gl.jiaoyubao.cn' target='_blank'>桂林培训</a> <a href='http://www.aiweisheji.com' target='_blank'>聊城网站建设</a> <a href='http://wsi.tantuw.com' target='_blank'>华尔街英语</a> <a href='http://www.hzcrjy.com' target='_blank'>杭州成人高考</a> <a href='http://www.jygcg.com' target='_blank'>苏州留学中介</a> <a href='http://www.jnukaoyan.com/' target='_blank'>暨南大学考研网</a> <a href='http://www.abroadusa.com/' target='_blank'>美国留学申请</a> </center></div>
+</div>
+
+</div>
+</div>
+
+ <script language="javascript">
+ function addbaoming()
+ {
+	 if($('#truename').val()=='')
+	 {
+		 alert('姓名不能为空');
+		 $('#truename').focus();
+		 return false;
+	}
+	if($('#tel').val()==''&&('#qq').val()=='')
+	 {
+		 alert('电话和qq不能同时为空');
+		 $('#tel').focus();
+		 return false;
+	}
+	if($('#tel').val()!="")
+	{
+			_patten1 = /^1\d{10}$/;
+                    _patten2 = /^(([0\+]\d{2,3}-)?(0\d{2,3})-)?(\d{7,8})(-(\d{2,5}))?$/;
+                    if (!_patten1.test($('#tel').val()) && !_patten2.test($('#tel').val())) {
+                        alert('电话格式不正确');
+            $('#tel').focus();
+            return false;
+                    }
+		}
+}
+ </script>
+        
+<script type="text/javascript" src="/Public/css+js/jquery-1.9.1.min.js"></script>
+<script type="text/javascript" src="/Public/css+js/Marquee.js"></script>
+<script type="text/javascript" charset="utf-8" src="http://static.bshare.cn/b/buttonLite.js#style=-1&uuid=&pophcol=2&lang=zh"></script><script type="text/javascript" charset="utf-8" src="http://static.bshare.cn/b/bshareC0.js"></script>
+
+<script type="text/javascript">
+$(function(){
+	
+	$('.marqueebox #marquee3').kxbdSuperMarquee({
+		distance:1384,
+		time:4,
+		navId:'#mar3Nav',
+		direction:'left'
+	});
+});
+</script>
+
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?3aa8612c389368add4fb72bd46cb2949";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
+
+
+<script>window._bd_share_config={"common":{"bdSnsKey":{},"bdText":"","bdMini":"1","bdMiniList":false,"bdPic":"","bdStyle":"0","bdSize":"16"},"slide":{"type":"slide","bdImg":"0","bdPos":"right","bdTop":"250"},"image":{"viewList":["qzone","tsina","tqq","renren","weixin"],"viewText":"分享到：","viewSize":"16"},"selectShare":{"bdContainerClass":null,"bdSelectMiniList":["qzone","tsina","tqq","renren","weixin"]}};with(document)0[(getElementsByTagName('head')[0]||body).appendChild(createElement('script')).src='http://bdimg.share.baidu.com/static/api/js/share.js?v=89860593.js?cdnversion='+~(-new Date()/36e5)];</script>
+
+<script type="text/javascript">
+var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F3aa8612c389368add4fb72bd46cb2949' type='text/javascript'%3E%3C/script%3E"));
+</script>
+
+<script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1260858521'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s11.cnzz.com/z_stat.php%3Fid%3D1260858521%26show%3Dpic' type='text/javascript'%3E%3C/script%3E"));</script>
+
+</body>
+</html>
